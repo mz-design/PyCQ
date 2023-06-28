df6544098b6132b2f97b6dc1e5c40fec291570be
Michael Zhenin <94615902+mz-design@users.noreply.github.com>
2023-06-28 10:48:39 +0300

    commit 27.06.2023

--- announcer.py
+++ announcer.py
@@ -15,28 +15,37 @@ from logger import Logger
 # Initialize logging
 logger = Logger(constants.LOG_FILE, level=constants.LOGGING_LEVEL)
 
+# Define global variables on module level
+my_ip = ''
+
 
 def announce_service(port, magic, interval):
+    global my_ip
     s = socket(AF_INET, SOCK_DGRAM)  # create UDP socket
-    s.bind(('', 0))
-    s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)  # this is a broadcast socket
-    my_ip = gethostbyname(gethostname())  # get our IP.
-    # (be careful of multiple NW interfaces or IPs)
+    try:
+        s.bind(('', 0))
+        s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)  # this is a broadcast socket
+        my_ip = gethostbyname(gethostname())  # get our IP.
+        # (be careful of multiple NW interfaces or IPs)
+        logger.add_log_entry(logging.DEBUG, f"announcer UDP socket bind successful")
+    except Exception as e:
+        # print(f"announcer UDP socket bind error occurred: {e}")
+        logger.add_log_entry(logging.ERROR, f"announcer UDP socket bind error occurred: {e}")
 
     try:
         while True:
             data = magic + my_ip + gethostname()
             s.sendto(data.encode('utf-8'), ('<broadcast>', port))
             # print("Sent service announcement")
-            logger.add_log_entry(logging.DEBUG, "Sent service announcement")
+            logger.add_log_entry(logging.DEBUG, f"Sent service announcement. Announcement interval is {interval}")
             sleep(interval)
     except Exception as e:
         # print(f"error occurred: {e}")
-        logger.add_log_entry(logging.CRITICAL, f"announcer - error occurred: {e}")
+        logger.add_log_entry(logging.CRITICAL, f"announcement send error occurred: {e}")
         s.close()
 
 # Usage Example:
-# tcp_port = 50000
+# port = 50000
 # magic = constants.MAGIC
 # interval = constants.ANNOUNCE_INTERVAL
 # announce_service(tcp_port, magic, interval)
--- listener.py
+++ listener.py
@@ -14,29 +14,37 @@ from logger import Logger
 # Initialize logging
 logger = Logger(constants.LOG_FILE, level=constants.LOGGING_LEVEL)
 
+# Define module global variables
+caller_ip = ''
+caller_hostname = ''
+s = None
+
 
 def listen_for_service(udp_port, udp_magic):
-    s = socket(AF_INET, SOCK_DGRAM)  # create UDP socket
-    s.bind(('', udp_port))
-    caller_ip = None
-    caller_hostname = None
+    global caller_ip, caller_hostname, s
+    try:
+        s = socket(AF_INET, SOCK_DGRAM)  # create UDP socket
+        s.bind(('', udp_port))
+        caller_ip = None
+        caller_hostname = None
+    except Exception as e:
+        # print(f"listener UDP socket bind error occurred: {e}")
+        logger.add_log_entry(logging.ERROR, f"listener UDP socket bind error occurred: {e}")
 
     while True:
         try:
             data, addr = s.recvfrom(1024)  # wait for a packet
         except Exception as e:
-            # print(f"Error receiving packet: {e}")
-            logger.add_log_entry(logging.ERROR, f"Error receiving announcement packet: {e}")
+            # print(f"listener: Error {e} receiving UDP packet on port {udp_port}")
+            logger.add_log_entry(logging.ERROR, f"listener: Error {e} receiving UDP packet on port {udp_port}")
             continue
 
         if data.decode('utf-8').startswith(udp_magic):
             caller_ip = addr[0]
-            caller_hostname = data[len(udp_magic)+len(caller_ip):].decode('utf-8')
-            logger.add_log_entry(logging.INFO, f"Got service announcement from {caller_ip} {caller_hostname}")
+            caller_hostname = data[len(udp_magic) + len(caller_ip):].decode('utf-8')
+            logger.add_log_entry(logging.INFO, f"Got new service announcement from {caller_ip} {caller_hostname}")
             break
 
     # close the listening UDP socket
     s.close()
     return caller_ip, caller_hostname
-
-
